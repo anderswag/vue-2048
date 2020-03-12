f4b30b6b31a5c59b98f06ed0cb1e53140cffbc41
anderswag <anderson.hung@jumbleberry.com>
2020-03-12 14:32:10 -0400

    add basic styling and structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="board-container">
+      <div v-for="(tile, index) in board.flat()" :key="index" class="board-tile"> {{tile}} </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      board: [
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, 2, 2, null]
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
+  display: flex;
+  justify-content: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.board-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 15px;
+  height: 500px;
+  width: 500px;
+  border: 5px solid rgba(255, 210, 126, 0.89);
+}
+
+.board-tile {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  font-size: 36px;
+  font-weight: bold;
 }
 </style>
